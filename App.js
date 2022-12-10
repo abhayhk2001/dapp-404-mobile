3c33567efe90a47ff2508eeed8ade9a94ef739a2
Abhay H Kashyap <abhayastro1004@gmail.com>
2022-12-10 11:07:19 +0530

    AuthStack Updated with React Native Elements

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-// import { Button, Text, View } from 'react-native';
+import React, { useState, useEffect, useMemo } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AuthContext } from "./src/context/AuthContext";
+import { ThemeProvider, createTheme } from '@rneui/themed';
+
 
 import {
-  ConfirmPost, Dashboard, Login, Maximised, NewPost, Profile, Signup
+  ConfirmPost, Dashboard, Splash, Login, Maximised, NewPost, Profile, Signup
 } from "./src/screens/allScreens";
 
+
+const AuthStack = createNativeStackNavigator();
 const NewPostStack = createNativeStackNavigator();
+const DashboardStack = createNativeStackNavigator();
+const ProfileStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const AuthStackScreen = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen
+      name="SignIn"
+      component={Login}
+      options={{
+        title: "Sign In",
+        headerStyle: {
+          backgroundColor: "#40d1ff",
+        },
+      }}
+    />
+  </AuthStack.Navigator>
+);
 
 function NewPostStackScreen() {
   return (
@@ -18,8 +41,6 @@ function NewPostStackScreen() {
   );
 }
 
-const DashboardStack = createNativeStackNavigator();
-
 function DashboardStackScreen() {
   return (
     <DashboardStack.Navigator>
@@ -29,8 +50,6 @@ function DashboardStackScreen() {
   );
 }
 
-const ProfileStack = createNativeStackNavigator();
-
 function ProfileStackScreen() {
   return (
     <ProfileStack.Navigator>
@@ -39,16 +58,98 @@ function ProfileStackScreen() {
   );
 }
 
-const Tab = createBottomTabNavigator();
+function MainTabScreen() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="NewPost" component={NewPostStackScreen} />
+      <Tab.Screen name="Dashboard" component={DashboardStackScreen} />
+      <Tab.Screen name="Profile" component={ProfileStackScreen} />
+    </Tab.Navigator>
+  );
+}
+
+
+const RootStack = createNativeStackNavigator();
+const RootStackScreen = ({ userToken }) => (
+  <RootStack.Navigator screenOptions={{ headerShown: false }}>
+    {userToken ? (
+      <RootStack.Screen
+        name="App"
+        component={MainTabScreen}
+        options={{
+          animationEnabled: false,
+        }}
+      />
+    ) : (
+      <RootStack.Screen
+        name="Auth"
+        component={AuthStackScreen}
+        options={{
+          animationEnabled: false,
+        }}
+      />
+    )}
+  </RootStack.Navigator>
+);
+
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState(false);
+
+  const myTheme = createTheme({
+    lightColors: {
+      primary: '#f2f2f2',
+    },
+    darkColors: {
+      primary: '#121212',
+      secondary: '#3658D7',
+      background: '#272547',
+      white: '#D1D3DB',
+      success: '#0ACDA4',
+      error: '#FF6058',
+      warning: '#FE9153'
+    },
+    mode: 'dark',
+  });
+
+  const authContext = useMemo(() => {
+    return {
+      signIn: (token) => {
+        setIsLoading(false);
+        setUserToken(token);
+      },
+      signUp: () => {
+        setIsLoading(false);
+        setUserToken("faculty-sudha123");
+      },
+      signOut: () => {
+        setIsLoading(false);
+        setUserToken(null);
+      },
+      isLoggedIn: () => {
+        return true
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  if (isLoading) {
+    return <Splash />;
+  }
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="NewPost" component={NewPostStackScreen} />
-        <Tab.Screen name="Dashboard" component={DashboardStackScreen} />
-        <Tab.Screen name="Profile" component={ProfileStackScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      <ThemeProvider theme={myTheme}>
+        <NavigationContainer>
+          <RootStackScreen userToken={userToken} />
+        </NavigationContainer>
+      </ThemeProvider>
+    </AuthContext.Provider>
   );
 }
--- src/screens/allScreens.js
+++ src/screens/allScreens.js
@@ -1,10 +1,10 @@
 import ConfirmPost from "./ConfirmPost";
 import Dashboard from "./Dashboard";
-// import Splash from "./Loading";
+import Splash from "./Loading";
 import Login from "./Login";
 import Maximised from "./Maximised";
 import NewPost from "./NewPost";
 import Profile from "./Profile";
 import Signup from "./Signup";
 
-export { ConfirmPost, Dashboard, Login, Maximised, NewPost, Profile, Signup };
+export { ConfirmPost, Dashboard, Splash, Login, Maximised, NewPost, Profile, Signup };
